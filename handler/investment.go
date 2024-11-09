1cdfede8ab026c8e3234bb53bda7857074b92b17
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2024-11-09 23:36:18 +0700

    refactor

--- handler/investment.go
+++ handler/investment.go
@@ -1,10 +1,11 @@
 package handler
 
 import (
+	"github.com/go-chi/chi/v5"
 	"net/http"
 	"project/lib"
-	"project/model"
 	"project/service"
+	"strconv"
 )
 
 type InvestmentHandler struct {
@@ -16,9 +17,22 @@ func InitInvestmentHandler(investmentService service.InvestmentService) Investme
 }
 
 func (handler InvestmentHandler) All(w http.ResponseWriter, r *http.Request) {
-	lib.JsonResponse(w).Success(0, "", model.Investment{})
+	lib.JsonResponse(w).Success(0, "", nil)
 }
 
 func (handler InvestmentHandler) Get(w http.ResponseWriter, r *http.Request) {
-	lib.JsonResponse(w).Success(0, "", model.Depreciation{})
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		lib.JsonResponse(w).Fail(http.StatusBadRequest, "Invalid Item ID")
+		return
+	}
+
+	depreciation, err := handler.InvestmentService.Get(id)
+	if err != nil {
+		lib.JsonResponse(w).Fail(0, "Barang tidak ditemukan")
+		return
+	}
+	lib.JsonResponse(w).Success(0, "", depreciation)
+
+	//lib.JsonResponse(w).Success(0, "", model.Depreciation{})
 }
