ae96edf8240721042b91cfe704a8dd5b477a01ef
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2024-11-11 07:42:55 +0700

    create items

--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.23.2
 require github.com/lib/pq v1.10.9
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require github.com/google/uuid v1.6.0 // indirect
--- handler/item.go
+++ handler/item.go
@@ -2,11 +2,17 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"project/lib"
 	"project/model"
 	"project/service"
 	"strconv"
+	"strings"
 )
 
 type ItemHandler struct {
@@ -18,7 +24,32 @@ func InitItemHandler(itemService service.ItemService) ItemHandler {
 }
 
 func (handler ItemHandler) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		lib.JsonResponse(w).Fail(http.StatusUnprocessableEntity, "File size too large (Max 10MB)")
+		return
+	}
 
+	file, fileHandler, err := r.FormFile("photo")
+	if err != nil {
+		lib.JsonResponse(w).Fail(http.StatusUnprocessableEntity, "Unable to upload file")
+		return
+	}
+	defer file.Close()
+	fileExtension := fileHandler.Filename[strings.LastIndex(fileHandler.Filename, "."):]
+	renamed := filepath.Join("uploads", uuid.New().String()+fileExtension)
+	destination, err := os.Create(renamed)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, file)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func (handler ItemHandler) All(w http.ResponseWriter, r *http.Request) {
@@ -34,7 +65,7 @@ func (handler ItemHandler) All(w http.ResponseWriter, r *http.Request) {
 		lib.JsonResponse(w).Fail(http.StatusBadRequest, "Invalid Page")
 		return
 	}
-	//log.Println(page)
+
 	limit := 10
 	if q := r.URL.Query().Get("limit"); q != "" {
 		limit, err = strconv.Atoi(q)
